5696c38669b9394483fa6d1f6d38937ad4f135ca
IE Dev 2 <ie.dev.2@hotmail.com>
2024-09-09 10:05:27 -0700

    EnMayuscula Ahora utiliza split y forEach

--- EnMayuscula.java
+++ EnMayuscula.java
@@ -5,20 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         String texto = lector.nextLine(), textoConMayusculas = "";
-        String caracterAuxiliar;
         lector.close();
-        for (int k = 0; k < texto.length(); k++) {
-            if (k < 1) {
-                caracterAuxiliar = texto.substring(k, k + 1).toUpperCase();
-            } else {
-                if (texto.substring(k - 1, k).equals(" ")) {
-                    caracterAuxiliar = texto.substring(k, k + 1).toUpperCase();
-                } else {
-                    caracterAuxiliar = texto.substring(k, k + 1);
-                }
-            }
-            textoConMayusculas += caracterAuxiliar;
+        String[] palabras = texto.split(" ");
+        for (String palabra : palabras){
+            textoConMayusculas=palabra.substring(0,1).toUpperCase()+palabra.substring(1);
+            System.out.print(textoConMayusculas+" ");
         }
-        System.out.print(textoConMayusculas);
     }
 }
